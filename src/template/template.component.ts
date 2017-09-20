118086fb992a81213615b192768821c1a6566c38
Cyrille <cyrille.rieau@gmail.com>
2017-09-20 17:27:14 +0200

    Commit Objet

--- src/template/template.component.ts
+++ src/template/template.component.ts
@@ -3,10 +3,12 @@ import {ClassOpe} from './interface';
 
 @Component({
     selector: 'app-template',
-    templateUrl:'./template.component.html'
+    templateUrl:'./template.component.html',
+    styleUrls: ['./template.component.css']
 })
 export class Operation {
     titre = "Cahier de Budgetisation";
+    budget= [];
     operation : ClassOpe = {
     date: new Date, 
     description:'',
@@ -17,4 +19,11 @@ export class Operation {
     afficher() {
         console.log(this.operation);
     }
+    add(){
+        this.budget.push(Object.assign({},this.operation));
+        console.log(this.budget);
+    };
+    delete(index:number){
+        this.budget.splice(index, 1);
+    };
 }
